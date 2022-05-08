65bedbeaa8aec229a765bcf73e666a93adc384f3
hashiguchi1544 <hashiguchi1544@gmail.com>
2022-05-08 09:34:38 +0900

    sample web site

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "世界")
+}
+
+func main() {
+	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+	e.GET("/hello", Hello)
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
